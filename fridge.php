a38bdaf850f0154da9b90eb20fbb8ad84d956c29
Adam Lassy <adamlassy@gmail.com>
2012-05-07 02:02:30 -0400

    Fridge test

--- fridge.php
+++ fridge.php
@@ -1,5 +1,12 @@
 <?php
-$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
 
+if ($_GET["w"])
+{
+	$arr = array('status' => 0);
+}
+else
+{
+	$arr = array('a' => 1, 'b' => 2, 'c' => 3, 'd' => 4, 'e' => 5);
+}
 echo json_encode($arr);
 ?>
