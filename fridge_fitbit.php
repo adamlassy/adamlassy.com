c6222ada83e5c6a72dc1cedecfadd443ded5df54
Adam Lassy <adamlassy@gmail.com>
2012-06-07 11:51:13 -0400

    debug

--- fridge_fitbit.php
+++ fridge_fitbit.php
@@ -24,7 +24,9 @@ function get_calories()
     $conssec = '79212ddbed1d4e3a846e6a6c32b11db0';
 
     // Fitbit API call (get activities for specified date)
-    $apiCall = "http://api.fitbit.com/1/user/-/activities/date/2011-01-25.xml";
+    $dat = date('Y-m-d');
+    //$apiCall = "http://api.fitbit.com/1/user/-/activities/date/" . $dat . ".xml";
+    $apiCall = "http://api.fitbit.com/1/user/-/activities/date/2012-05-08.xml";
 
     // Start session to store the information between calls
     session_start();
